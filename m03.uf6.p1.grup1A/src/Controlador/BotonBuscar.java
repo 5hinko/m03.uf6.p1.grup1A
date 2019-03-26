8cb0f20d7bdf652c051078c8f27d2d3e649a019e
5hinko <se4bzhang@gmail.com>
2019-03-26 14:04:35 +0100

    Con PreparedStatement

--- m03.uf6.p1.grup1A/src/Controlador/BotonBuscar.java
+++ m03.uf6.p1.grup1A/src/Controlador/BotonBuscar.java
@@ -15,6 +15,8 @@ import static Vista.TablaInfo.jTablaInfo;
 import static Vista.TablaInfo.jTxtFieldBusca;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.logging.Level;
@@ -27,59 +29,101 @@ import javax.swing.table.TableModel;
  */
 public class BotonBuscar implements ActionListener {
 
+    private Connection con = null;
+    private ResultSet resultat = null;
+    private PreparedStatement statement = null;
+    private String sQuery;
+
     public BotonBuscar() {
     }
 
     @Override
     public void actionPerformed(ActionEvent e) {
 
-        String sQuery = jTxtFieldBusca.getText().toString();
+        sQuery = jTxtFieldBusca.getText().toString();
         if (sQuery.length() > 0) {
-            ResultSet resultat = selectQuery(sQuery);
-            TableModel model = new ModeloDeTablaSimple(resultat);
-            actualitzaTaula(jTablaInfo, model);
+
+            try {
+                con = Connexion.getConnection();
+                resultat = selectQuery();
+                statement.executeUpdate();
+                TableModel model = new ModeloDeTablaSimple(resultat);
+                actualitzaTaula(jTablaInfo, model);
+            } catch (SQLException ex) {
+                Logger.getLogger(BotonBuscar.class.getName()).log(Level.SEVERE, null, ex);
+            }
         } else {
-            TablaInfo.cambiosEnComboBox();
+            TablaInfo.selectAllInTabla();
         }
     }
 
-    private ResultSet selectQuery(String sBuscar) {
-        ResultSet resultat = null;
-        String sQuery;
+    private ResultSet selectQuery() throws SQLException {
         EnumTablas tablaEnum = EnumTablas.getEnum(itemCheck);
         switch (tablaEnum) {
             case Malalties:
-                sQuery = ("SELECT * "
-                        + " FROM " + tablaEnum
-                        + " WHERE codi = " + sBuscar + " OR "
-                        + "nom LIKE '%" + sBuscar + "%' OR "
-                        + "tratamiento LIKE '%" + sBuscar + "%' OR "
-                        + "duradaTratamentDies = " + sBuscar);
-                break;
+                sQuery = ("SELECT * FROM " + tablaEnum + " WHERE codi = ? OR nom LIKE ? OR tratamiento LIKE ? OR duradaTratamentDies = ?");
+
+                statement = con.prepareStatement(sQuery);
+
+                try {
+                    try {
+                        statement.setInt(1, Integer.parseInt(sQuery));
+                    } catch (Exception e) {
+
+                    }
+                    statement.setString(2, "%" + sQuery + "%");
+                    statement.setString(3, "%" + sQuery + "%");
+                    try {
+                        statement.setInt(4, Integer.parseInt(sQuery));
+                    } catch (Exception e) {
+
+                    }
+                } catch (SQLException ex) {
+                    Logger.getLogger(BotonBuscar.class.getName()).log(Level.SEVERE, null, ex);
+                }
+
             case Visita:
-                sQuery = ("SELECT * "
-                        + " FROM " + tablaEnum
-                        + " WHERE dataVisita '%" + sBuscar + "%' OR "
-                        + "nomMalaltia '%" + sBuscar + "%' OR "
-                        + "dniPacient '%" + sBuscar + "%' OR "
-                        + "dniMetges '%" + sBuscar + "%'");
-                break;
+                sQuery = ("SELECT * FROM " + tablaEnum + " WHERE dataVisita LIKE ? OR "
+                        + "nomMalaltia LIKE ? OR "
+                        + "dniPacient LIKE ? OR "
+                        + "dniMetges LIKE ?");
+
+                statement = con.prepareStatement(sQuery);
+
+                try {
+                    statement.setString(1, "%" + sQuery + "%");
+                    statement.setString(2, "%" + sQuery + "%");
+                    statement.setString(3, "%" + sQuery + "%");
+                    statement.setString(4, "%" + sQuery + "%");
+
+                } catch (SQLException ex) {
+                    Logger.getLogger(BotonBuscar.class.getName()).log(Level.SEVERE, null, ex);
+                }
+
             default:
                 sQuery = ("SELECT * "
-                        + " FROM " + tablaEnum
-                        + " WHERE nom LIKE '%" + sBuscar + "%' OR "
-                        + "cognom1 LIKE '%" + sBuscar + "%' OR "
-                        + "cognom2 LIKE '%" + sBuscar + "%' OR "
-                        + "DNI LIKE '%" + sBuscar + "%' OR "
-                        + "numSS LIKE '%" + sBuscar + "%'");
-                break;
-        }
-        try {
-            resultat = Connexion.consultaBBDD(sQuery);
-        } catch (SQLException ex) {
-            Logger.getLogger(TablaInfo.class.getName()).log(Level.SEVERE, null, ex);
-        }
+                        + " FROM " + tablaEnum + " WHERE nom LIKE ? OR "
+                        + "cognom1 LIKE ? OR "
+                        + "cognom2 LIKE ? OR "
+                        + "DNI LIKE ? OR "
+                        + "numSS LIKE ?");
 
-        return resultat;
+                statement = con.prepareStatement(sQuery);
+
+                try {
+                    statement.setString(1, "%" + sQuery + "%");
+                    statement.setString(2, "%" + sQuery + "%");
+                    statement.setString(3, "%" + sQuery + "%");
+                    statement.setString(4, "%" + sQuery + "%");
+                    statement.setString(5, "%" + sQuery + "%");
+
+                } catch (SQLException ex) {
+                    Logger.getLogger(BotonBuscar.class.getName()).log(Level.SEVERE, null, ex);
+                }
+
+        }
+        statement.executeQuery();
+        
+        return statement.getResultSet();
     }
 }
--- m03.uf6.p1.grup1A/src/Modelo/ModeloDeTablaSimple.java
+++ m03.uf6.p1.grup1A/src/Modelo/ModeloDeTablaSimple.java
@@ -1,17 +1,18 @@
 package Modelo;
 
+import Vista.TablaInfo;
 import java.sql.*;
 import java.util.ArrayList;
 import javax.swing.table.AbstractTableModel;
 
 public class ModeloDeTablaSimple extends AbstractTableModel {
-    
+
     private ArrayList<String> nombreCampos;
     private ArrayList<ArrayList> registros;
-    
+
     public ModeloDeTablaSimple(ResultSet results) {
         super();
-        try{
+        try {
             ResultSetMetaData rsm = results.getMetaData();
             int numCampos = rsm.getColumnCount();
             nombreCampos = new ArrayList();
@@ -19,55 +20,78 @@ public class ModeloDeTablaSimple extends AbstractTableModel {
                 nombreCampos.add(rsm.getColumnName(i));
             }
             registros = new ArrayList();
-            while(results.next()){
+            while (results.next()) {
                 ArrayList<String> campos = new ArrayList();
-                for(int j=1; j<=numCampos;j++ ){
+                for (int j = 1; j <= numCampos; j++) {
                     campos.add(results.getString(j));
                 }
                 registros.add(campos);
             }
             results.close();
-        }catch(SQLException e){
-          e.printStackTrace();
-      }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
+
     @Override
     public int getColumnCount() {
-      return nombreCampos.size();
+        return nombreCampos.size();
     }
+
     @Override
     public int getRowCount() {
-      return registros.size();
+        return registros.size();
     }
-    
+
     @Override
     public void setValueAt(Object value, int row, int col) {
         ArrayList aux = registros.get(row);
-        aux.set(col,(String)value);
+        aux.set(col, (String) value);
+        //Metodo que actualiza
         fireTableCellUpdated(row, col);
     }
 
     @Override
     public String getColumnName(int col) {
-      return nombreCampos.get(col);
+        return nombreCampos.get(col);
     }
 
     @Override
     public Class getColumnClass(int col) {
         return String.class;
     }
-    
+
     @Override
     public Object getValueAt(int row, int col) {
-         return registros.get(row).get(col);
+        return registros.get(row).get(col);
     }
 
     @Override
     public boolean isCellEditable(int rowIndex, int columnIndex) {
+
+        switch (EnumTablas.getEnum(TablaInfo.itemCheck)) {
+            case Malalties:
+                if (columnIndex == 0) {
+                    return false;
+                }
+                break;
+            case Metges:
+                if (columnIndex == 3) {
+                    return false;
+                }
+                break;
+            case Pacients:
+                if (columnIndex == 3) {
+                    return false;
+                }
+                break;
+            case Visita:
+                if (columnIndex == 0 || columnIndex == 2) {
+                    return false;
+                }
+                break;
+        }
         return true;
     }
-    
+
 }
-
-
-
