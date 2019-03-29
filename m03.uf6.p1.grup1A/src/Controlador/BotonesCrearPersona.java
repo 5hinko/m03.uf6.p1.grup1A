e8f7ed83a3edac08b4be685c2959aa9ae8c78022
Lorenzo <Lorenzo@192.168.12.29>
2019-03-29 08:42:27 +0100

    Yo voy haciendo comprobaciones, es mi rollo

--- m03.uf6.p1.grup1A/src/Controlador/BotonesCrearPersona.java
+++ m03.uf6.p1.grup1A/src/Controlador/BotonesCrearPersona.java
@@ -7,11 +7,15 @@ package Controlador;
 
 import Modelo.ComprobacionesPersona;
 import Modelo.InsertPersona;
+import Modelo.ProcedimientosPersona;
 import Vista.ErrorInsert;
 import static Vista.FormularioInsert.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.MouseAdapter;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
@@ -61,32 +65,42 @@ public class BotonesCrearPersona extends MouseAdapter implements ActionListener 
 
             //Un metodo para mirar en la base de datos si la clase primaria ya existe.
             errors += comprobaciones(data);
-            
+
         } catch (NullPointerException e) {
             errors = "Los campos no pueden estar vacios.";
             fallo = true;
+            ErrorInsert.infoBox(errors, "ERROR");
         }
 
-       /* if (fallo||errors != null) {
-            ErrorInsert.infoBox(errors, "Error");
-        } else {
-            
-                ErrorInsert.infoBox("muy bien", "Bien");
-            }
-        */
-       
-        InsertPersona.Crear(data);
-       
-        /*
         try {
+            /* if (fallo||errors != null) {
+            ErrorInsert.infoBox(errors, "Error");
+            } else {
+            
+            ErrorInsert.infoBox("muy bien", "Bien");
+            }
+             */
+            if (ProcedimientosPersona.existePaciente(data[3])) {
+                ErrorInsert.infoBox("Ya existe un paciente/medico con ese DNI", "Error");
+            } else if (ProcedimientosPersona.existeSS(data[4])) {
+                ErrorInsert.infoBox("Ya existe un paciente/medico con ese numero de SS", "Error");
+            } else {
+                 ErrorInsert.infoBox("Bien", "Error");
+            }
+
+            /*
+            try {
             GestioHospital.controladorCreaPersona(data, mode);
 
             new Mostra("CORRECTE", "La persona s'ha afegit correctament.");
             JFwindow.dispose();
-        } catch (Exception e) {
+            } catch (Exception e) {
             new Mostra("ERROR", "No s'ha pogut afegir la persona per les següents raons:" + e.getMessage());
+            }
+             */
+        } catch (SQLException ex) {
+            Logger.getLogger(BotonesCrearPersona.class.getName()).log(Level.SEVERE, null, ex);
         }
-         */
     }
 
     public String comprobaciones(String[] data) {
