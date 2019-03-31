b1ce0bf3283bd8634ac01137217b0dc03fdfdb33
5hinko <se4bzhang@gmail.com>
2019-03-31 16:44:05 +0200

    A Connexion añadido un método para otro usuario

--- m03.uf6.p1.grup1A/src/Controlador/TablaInfoAction.java
+++ m03.uf6.p1.grup1A/src/Controlador/TablaInfoAction.java
@@ -27,7 +27,7 @@ public class TablaInfoAction implements TableModelListener {
 
     public TablaInfoAction() {
     }
-    
+
     private int fila;
     private PreparedStatement sentencia = null;
     private Connection con = null;
@@ -35,75 +35,88 @@ public class TablaInfoAction implements TableModelListener {
 
     @Override
     public void tableChanged(TableModelEvent e) {
+        boolean commited = false;
         fila = e.getFirstRow();
         dades = jTablaInfo.getModel();
         try {
-            con = Connexion.getConnection();
-            updateQuery();
+            //Actualiza Solo debería hacerlo el Admin
+            con = Connexion.getConnectionAdmin();
+            con.setAutoCommit(false);
+            updateQueryPreparedStatement();
+            con.commit();
             sentencia.executeUpdate();
+            commited = true;
         } catch (SQLException ex) {
-            Logger.getLogger(TablaInfo.class.getName()).log(Level.SEVERE, null, ex);
+                Logger.getLogger(TablaInfoAction.class.getName()).log(Level.SEVERE, null, ex);
+        } finally {
+            try {
+                if (con != null) {
+                    if (!commited) {
+                        con.rollback();
+                    }
+                    con.close();
+                }
+                con.close();
+            } catch (SQLException ex) {
+                Logger.getLogger(TablaInfoAction.class.getName()).log(Level.SEVERE, null, ex);
+            }
         }
     }
 
-    private void updateQuery() {
-        try {
-            EnumTablas tablaEnum = EnumTablas.getEnum(itemCheck);
-            String consulta = "UPDATE " + tablaEnum;
-            switch (tablaEnum) {
-                case Malalties:
-                    consulta += " SET nom = ?, causaBaixa = ?, tratamiento = ?, duradaTratamentDies = ? WHERE codi = ?";
-                    sentencia = con.prepareStatement(consulta);
-                    sentencia.setString(1, (String) (dades.getValueAt(fila, 1)));
-                    sentencia.setInt(2, Integer.parseInt(dades.getValueAt(fila, 2).toString()));
-                    sentencia.setString(3, (String) (dades.getValueAt(fila, 3)));
-                    sentencia.setInt(4, Integer.parseInt(dades.getValueAt(fila, 4).toString()));
-                    sentencia.setInt(5, Integer.parseInt(dades.getValueAt(fila, 0).toString()));
-                    break;
-                case Visita:
-                    consulta += " SET nomMalaltia = ?, dniMetges = ? , informe = ? WHERE dataVisita = ? AND dniPacient = ?";
-                    sentencia = con.prepareStatement(consulta);
-                    sentencia.setString(1, (String) (dades.getValueAt(fila, 1)));
-                    sentencia.setString(2, (String) (dades.getValueAt(fila, 3)));
-                    sentencia.setString(3, (String) (dades.getValueAt(fila, 4)));
-                    sentencia.setString(4, (String) (dades.getValueAt(fila, 0)));
-                    sentencia.setString(5, (String) (dades.getValueAt(fila, 2)));
-                    break;
-                case Pacients:
-                    consulta += " SET nom = ?, cognom1 = ?, cognom2 = ?, numSS = ?, telefon = ? , ciutat = ?, codipostal= ?, direccio = ? WHERE DNI = ?";
-                    sentencia = con.prepareStatement(consulta);
-                    sentencia.setString(1, (String) (dades.getValueAt(fila, 0)));
-                    sentencia.setString(2, (String) (dades.getValueAt(fila, 1)));
-                    sentencia.setString(3, (String) (dades.getValueAt(fila, 2)));
-                    sentencia.setString(4, (String) (dades.getValueAt(fila, 4)));
-                    sentencia.setString(5, (String) (dades.getValueAt(fila, 5)));
-                    sentencia.setString(6, (String) (dades.getValueAt(fila, 6)));
-                    sentencia.setInt(7, Integer.parseInt(dades.getValueAt(fila, 7).toString()));
-                    sentencia.setString(8, (String) (dades.getValueAt(fila, 8)));
-                    sentencia.setString(6, (String) (dades.getValueAt(fila, 3)));
-                    break;
-                case Metges:
-                    consulta += " SET nom = ?, cognom1 = ?, cognom2 = ?, numSS = ?, telefon = ?, ciutat = ?, codipostal = ?, direccio = ?, numEmpleat = ?, codiCC= ?, salariMensual = ? WHERE DNI = ?";
-                    sentencia = con.prepareStatement(consulta);
-                    sentencia.setString(1, (String) (dades.getValueAt(fila, 0)));
-                    sentencia.setString(2, (String) (dades.getValueAt(fila, 1)));
-                    sentencia.setString(3, (String) (dades.getValueAt(fila, 2)));
-                    sentencia.setString(4, (String) (dades.getValueAt(fila, 4)));
-                    sentencia.setString(5, (String) (dades.getValueAt(fila, 5)));
-                    sentencia.setString(6, (String) (dades.getValueAt(fila, 6)));
-                    sentencia.setInt(7, Integer.parseInt(dades.getValueAt(fila, 7).toString()));
-                    sentencia.setString(8, (String) (dades.getValueAt(fila, 8)));
-                    sentencia.setInt(9, Integer.parseInt(dades.getValueAt(fila, 9).toString()));
-                    sentencia.setString(10, (String) (dades.getValueAt(fila, 10)));
-                    sentencia.setInt(11, Integer.parseInt(dades.getValueAt(fila, 11).toString()));
-                    sentencia.setString(12, (String) (dades.getValueAt(fila, 3)));
+    private void updateQueryPreparedStatement() throws SQLException {
+        EnumTablas tablaEnum = EnumTablas.getEnum(itemCheck);
+        System.out.println("" + tablaEnum);
+        String consulta = "UPDATE " + tablaEnum;
+        switch (tablaEnum) {
+            case Malalties:
+                consulta += " SET nom LIKE ?, causaBaixa = ?, tratamiento LIKE ?, duradaTratamentDies = ? WHERE codi = ?";
+                sentencia = con.prepareStatement(consulta);
+                sentencia.setString(1, (String) (dades.getValueAt(fila, 1)));
+                sentencia.setInt(2, Integer.parseInt(dades.getValueAt(fila, 2).toString()));
+                sentencia.setString(3, (String) (dades.getValueAt(fila, 3)));
+                sentencia.setInt(4, Integer.parseInt(dades.getValueAt(fila, 4).toString()));
+                sentencia.setInt(5, Integer.parseInt(dades.getValueAt(fila, 0).toString()));
+                break;
+            case Visita:
+                consulta += " SET nomMalaltia LIKE ?, dniMetges LIKE ? , informe LIKE ? WHERE dataVisita LIKE ? AND dniPacient LIKE ?";
+                sentencia = con.prepareStatement(consulta);
+                sentencia.setString(1, (String) (dades.getValueAt(fila, 1)));
+                sentencia.setString(2, (String) (dades.getValueAt(fila, 3)));
+                sentencia.setString(3, (String) (dades.getValueAt(fila, 4)));
+                sentencia.setString(4, (String) (dades.getValueAt(fila, 0)));
+                sentencia.setString(5, (String) (dades.getValueAt(fila, 2)));
+                break;
+            case Pacients:
+                consulta += " SET nom LIKE ?, cognom1 LIKE ?, cognom2 LIKE ?, numSS LIKE ?, telefon LIKE ? , ciutat LIKE ?, codipostal = ?, direccio LIKE ? WHERE DNI LIKE ?";
+                sentencia = con.prepareStatement(consulta);
+                sentencia.setString(1, (String) (dades.getValueAt(fila, 0)));
+                sentencia.setString(2, (String) (dades.getValueAt(fila, 1)));
+                sentencia.setString(3, (String) (dades.getValueAt(fila, 2)));
+                sentencia.setString(4, (String) (dades.getValueAt(fila, 4)));
+                sentencia.setString(5, (String) (dades.getValueAt(fila, 5)));
+                sentencia.setString(6, (String) (dades.getValueAt(fila, 6)));
+                sentencia.setString(7, (String) (dades.getValueAt(fila, 7)));
+                sentencia.setString(8, (String) (dades.getValueAt(fila, 8)));
+                sentencia.setString(9, (String) (dades.getValueAt(fila, 3)));
+                break;
+            case Metges:
+                consulta += " SET nom LIKE ?, cognom1 LIKE ?, cognom2 LIKE ?, numSS LIKE ?, telefon LIKE ?, ciutat LIKE ?, codipostal = ?, direccio LIKE ?, codiCC LIKE ?, salariMensual = ? WHERE DNI LIKE ?";
+                sentencia = con.prepareStatement(consulta);
+                sentencia.setString(1, (String) (dades.getValueAt(fila, 0)));
+                sentencia.setString(2, (String) (dades.getValueAt(fila, 1)));
+                sentencia.setString(3, (String) (dades.getValueAt(fila, 2)));
+                sentencia.setString(4, (String) (dades.getValueAt(fila, 4)));
+                sentencia.setString(5, (String) (dades.getValueAt(fila, 5)));
+                sentencia.setString(6, (String) (dades.getValueAt(fila, 6)));
+                sentencia.setString(7, (String) (dades.getValueAt(fila, 7)));
+                sentencia.setString(8, (String) (dades.getValueAt(fila, 8)));
+                sentencia.setString(9, (String) (dades.getValueAt(fila, 9)));
+                sentencia.setInt(10, Integer.parseInt(dades.getValueAt(fila, 10).toString()));
+                sentencia.setString(11, (String) (dades.getValueAt(fila, 3)));
 
-                    break;
-                default:
-                    break;
-            }
-        } catch (SQLException ex) {
-            Logger.getLogger(TablaInfo.class.getName()).log(Level.SEVERE, null, ex);
+                break;
+            default:
+                break;
         }
     }
 }
